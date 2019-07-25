b25dfdf6287539cd7073aead6fcbb993dcc10033
Carole Chevalier <ateliercakocako@gmail.com>
2019-07-25 15:22:19 +0200

    TestDate solution

--- TestDate.java
+++ TestDate.java
@@ -1,8 +1,10 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 import java.util.Date;
-import java.util.Scanner; 
+import java.text.ParseException;
+
 
 class TestDate {
 
@@ -17,7 +19,12 @@ class TestDate {
         int year = sc.nextInt();
         sc.close();
 
-        // TODO : your code after this line
+        // afficher la date sous un format spécifique
         
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Calendar c = new GregorianCalendar(year, month -1, day);
+        System.out.println("The date is " + sdf.format(c.getTime()));        
+        System.out.println("The day of week is " + c.get(Calendar.DAY_OF_WEEK));
+
     }
 }
